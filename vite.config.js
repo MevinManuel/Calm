9becd3da8d0bd3e30770d7b15f0a5d79afd2594e
Mevin Manuel <Mevinmnl123@gmail.com>
2025-03-10 02:27:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/calm/', // Set this to the name of your GitHub repository
   plugins: [react()],
   build: {
     outDir: 'build', // Ensure the output directory is set to 'build'
